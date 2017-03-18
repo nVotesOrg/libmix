0ad0d348d3c85081f132fd1efcaf2920d7f6a478
ruescasd <fastness@gmail.com>
2017-03-18 15:24:01 +0100

    Fixed indentation

--- src/main/scala/org/nvotes/mix/Benchmark.scala
+++ src/main/scala/org/nvotes/mix/Benchmark.scala
@@ -7,9 +7,9 @@ import ch.bfh.unicrypt.math.algebra.general.classes.Pair
 import ch.bfh.unicrypt.math.algebra.general.classes.Tuple
 import org.nvotes.mix._
 
-/**	Simulates a two authority election for benchmarking purposes
+/** Simulates a two authority election for benchmarking purposes
  *
- * 	The sequence is
+ *  The sequence is
  *
  *  Share generation, share pok verification, public key generation, vote casting
  *  mixing, mix verification, decryption, decryption verification
@@ -17,10 +17,10 @@ import org.nvotes.mix._
  */
 object Benchmark extends App {
 
-	val totalVotes = 10
-	val proverId1 = "auth1"
-	val proverId2 = "auth2"
-	val group = GStarModSafePrime.getFirstInstance(2048)
+  val totalVotes = 10
+  val proverId1 = "auth1"
+  val proverId2 = "auth2"
+  val group = GStarModSafePrime.getFirstInstance(2048)
   val generator = group.getDefaultGenerator()
   val cSettings = CryptoSettings(group, generator)
 
@@ -33,19 +33,19 @@ object Benchmark extends App {
   val ok1 = Verifier.verifyKeyShare(share1, cSettings, proverId1)
   val ok2 = Verifier.verifyKeyShare(share2, cSettings, proverId2)
   if(!(ok1 && ok2)) {
-  	throw new Exception("failed to verify shares $ok1 $ok2")
+    throw new Exception("failed to verify shares $ok1 $ok2")
   }
 
   // create public key
   val shares = allShares.map { share =>
-  	Util.getPublicKeyFromString(share.keyShare, cSettings.generator)
+    Util.getPublicKeyFromString(share.keyShare, cSettings.generator)
   }
-	// the public key is the multiplcation of each share (or the addition of each exponent)
+  // the public key is the multiplcation of each share (or the addition of each exponent)
   val publicKey = shares.reduce( (a,b) => a.apply(b) )
   val publicKeyString = publicKey.convertToString
 
-	// create votes
-	val plaintexts = Seq.fill(totalVotes)(scala.util.Random.nextInt(1000))
+  // create votes
+  val plaintexts = Seq.fill(totalVotes)(scala.util.Random.nextInt(1000))
 
   // encrypt the votes with the public key of the election
   val votes = Util.encryptVotes(plaintexts, cSettings, publicKey).map(_.convertToString)
@@ -65,7 +65,7 @@ object Benchmark extends App {
   val oks2 = Verifier.verifyShuffle(Util.tupleFromSeq(votes1), Util.tupleFromSeq(votes2), mixTwo.shuffleProof,
     proverId2, publicKey, cSettings)
   if(!(oks1 && oks2)) {
-  	throw new Exception("failed to verify shuffles $oks1 $oks2")
+    throw new Exception("failed to verify shuffles $oks1 $oks2")
   }
 
   val decryption1 = KeyMakerTrustee.partialDecryption(proverId1, mixTwo.votes, private1, cSettings)
@@ -76,7 +76,7 @@ object Benchmark extends App {
   val okd1 = Verifier.verifyPartialDecryption(decryption1, votes2, cSettings, proverId1, shares(0))
   val okd2 = Verifier.verifyPartialDecryption(decryption2, votes2, cSettings, proverId2, shares(1))
   if(!(okd1 && okd2)) {
-  	throw new Exception(s"failed to verify decryptions $okd1 $okd2")
+    throw new Exception(s"failed to verify decryptions $okd1 $okd2")
   }
 
   // combine decryptions
@@ -86,44 +86,44 @@ object Benchmark extends App {
   // println(decrypted.map(_.toInt).sorted)
   println("Plaintexts match: " + (decrypted.map(_.toInt).sorted == plaintexts.sorted))
 
-  /**	Helper to combine decryptions and yield plaintexts.
+  /** Helper to combine decryptions and yield plaintexts.
    *
-   *	Combines the decryptions, applies them to the ciphertexts and
+   *  Combines the decryptions, applies them to the ciphertexts and
    *  finally decodes.
    */
-	def combineDecryptions(decryptions: Seq[PartialDecryptionDTO], mixedVotes: Seq[String],
-		cSettings: CryptoSettings) = {
+  def combineDecryptions(decryptions: Seq[PartialDecryptionDTO], mixedVotes: Seq[String],
+    cSettings: CryptoSettings) = {
 
-		val decryptionElements = decryptions.map(
-     	ds => ds.partialDecryptions.par.map(Util.fromString(cSettings.group, _)).seq
+    val decryptionElements = decryptions.map(
+      ds => ds.partialDecryptions.par.map(Util.fromString(cSettings.group, _)).seq
     )
 
-		val combined = decryptionElements.reduce { (a, b) =>
-			(a zip b).par.map(c => c._1.apply(c._2)).seq
+    val combined = decryptionElements.reduce { (a, b) =>
+      (a zip b).par.map(c => c._1.apply(c._2)).seq
     }
 
-		val elGamal = ElGamalEncryptionScheme.getInstance(cSettings.generator)
+    val elGamal = ElGamalEncryptionScheme.getInstance(cSettings.generator)
 
-		val votes = mixedVotes.par.map( v => Util.fromString(elGamal.getEncryptionSpace, v).asInstanceOf[Pair] ).seq
+    val votes = mixedVotes.par.map( v => Util.fromString(elGamal.getEncryptionSpace, v).asInstanceOf[Pair] ).seq
     // a^-x * b = m
     val decrypted = (votes zip combined).par.map(c => c._1.getSecond().apply(c._2)).seq
     val encoder = ZModPrimeToGStarModSafePrime.getInstance(cSettings.group)
 
     val plaintexts = decrypted.par.map(encoder.decode(_).convertToString).seq
-		plaintexts
-	}
+    plaintexts
+  }
 }
 
-/**	Represents a key maker trustee
+/** Represents a key maker trustee
  *
- * 	Methods to create shares and partially decrypt votes.
+ *  Methods to create shares and partially decrypt votes.
  *  Mixes in the nMix KeyMaker trait.
  */
 object KeyMakerTrustee extends KeyMaker {
 
-  /**	Creates a key share
+  /** Creates a key share
    *
-   * 	Returns the key share and proof of knowledge as an nMix EncryptionKeyShareDTO.
+   *  Returns the key share and proof of knowledge as an nMix EncryptionKeyShareDTO.
    *  Returns the private key part of the share as a unicrypted converted String
    */
   def createKeyShare(id: String, cSettings: CryptoSettings): (EncryptionKeyShareDTO, String) = {
@@ -133,12 +133,12 @@ object KeyMakerTrustee extends KeyMaker {
     (encryptionKeyShareDTO, privateKey)
   }
 
-  /**	Partially decrypt a ciphertext with the private part of a share
+  /** Partially decrypt a ciphertext with the private part of a share
    *
-   * 	Returns the partial decryption and proof of knowledge as an nMix EncryptionKeyShareDTO.
+   *  Returns the partial decryption and proof of knowledge as an nMix EncryptionKeyShareDTO.
    */
   def partialDecryption(id: String, votes: Seq[String],
-  	privateShare: String, cSettings: CryptoSettings): PartialDecryptionDTO = {
+    privateShare: String, cSettings: CryptoSettings): PartialDecryptionDTO = {
 
     val elGamal = ElGamalEncryptionScheme.getInstance(cSettings.generator)
     val v = votes.par.map( v => Util.fromString(elGamal.getEncryptionSpace, v).asInstanceOf[Pair]).seq
@@ -149,16 +149,16 @@ object KeyMakerTrustee extends KeyMaker {
 }
 
 
-/**	Represents a shuffling trustee
+/** Represents a shuffling trustee
  *
- * 	Methods to mix votes.
+ *  Methods to mix votes.
  *  Mixes in the nMix Mixer trait.
  */
 object MixerTrustee extends Mixer {
 
-	/**	Shuffle the provided votes
+  /** Shuffle the provided votes
    *
-   * 	Returns the shuffle and proof of knowledgeas an nMix ShuffleResultDTO
+   *  Returns the shuffle and proof of knowledgeas an nMix ShuffleResultDTO
    */
   def shuffleVotes(votes: Seq[String], publicKey: String, id: String, cSettings: CryptoSettings): ShuffleResultDTO = {
     println("Mixer shuffle..")
